16f329abc345cb75317cb88ef1092dc62f127219
Mayara de Oliveira Sant'Anna <159502755+mayarasantanna2@users.noreply.github.com>
2025-02-27 11:03:52 -0300

    Add files via upload

---
+++ aula03/exercicio01.php
@@ -0,0 +1,9 @@
+<?php 
+$nome1 = $_POST['nom'];
+$msg1 = $_POST['msg'];
+
+echo "Mensagem: $msg1 <br>";
+echo"Obrigado pela sua mensagem $nome1 ! <br>";
+
+
+?>
---
+++ aula03/processa01.php
@@ -0,0 +1,9 @@
+<?php 
+$nome = $_GET['cxnome'];
+$idade = $_GET['cxidade'];
+
+echo"Nome digitado: $nome <br>";
+echo"Idade digitada: $idade anos de vida! <br>";
+
+
+?>
